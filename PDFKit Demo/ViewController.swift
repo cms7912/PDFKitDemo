5881e956379d51d6a4e928c75661d97ff7081467
Clint Stephens <github@clint-stephens.com>
2020-03-17 07:58:16 -0500

    Initial Commit

---
+++ PDFKit Demo/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  PDFKit Demo
+//
+//  Created by Clint on 3/17/20.
+//  Copyright © 2020 CMRS Ventures. All rights reserved.
+//
+
+import UIKit
+import PDFKit
+
+// 1) import the 'PDFKit' framework
+
+
+class ViewController: UIViewController {
+	@IBOutlet weak var pdfView: PDFView!
+	@IBOutlet weak var pdfThumbnailView: PDFThumbnailView!
+
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// 'func viewDidLoad' runs when 'ViewController' first loads
+
+		// 2) Set path from which to load a PDF
+
+
+		// 3) Load the pdf by creating PDFDocument and the setting it to the pdfView's .document property
+
+
+		// 4) Connect thumbnail view to the pdfView
+
+	}
+
+
+	override func viewDidAppear(_ animated: Bool) {
+		// 'func viewDidAppear' runs after 'ViewController' loading is done and the view has been shown
+
+
+		// 5) Find phrase
+		// First conduct search for phrase
+		// Second move to that first item found, then set selection to item
+
+
+		// 6) Create an annotation on item.
+		// First get the current page
+		// Second create annotation
+		// Third add annotation to the current page
+
+
+
+	}
+}
+
